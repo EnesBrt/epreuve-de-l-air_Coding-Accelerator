acafb0248cbcd1d60fcd6c4042982245cf91071f
EnesBrt <enes.barut@gmx.com>
2023-04-15 19:57:54 +0200

    added air09.py file

---
+++ air09.py
@@ -0,0 +1,19 @@
+# Rotation vers la gauche
+
+import sys
+
+
+def to_the_left(sting_list):
+    index_order = [1, 2, 3, 0]
+    new_list = [sting_list[i] for i in index_order]
+    print(new_list)
+
+    for n in range(len(new_list)):
+        if n == len(new_list) - 1:
+            print(new_list[n], end='')
+        else:
+            print(new_list[n], end=', ')
+
+
+string_list = [arg for arg in sys.argv[1:]]
+to_the_left(string_list)
